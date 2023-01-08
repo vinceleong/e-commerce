d01558e942e6c97ac39bbc22250e32b4623dc288
vinceleong <61788459+vinceleong@users.noreply.github.com>
2023-01-08 19:09:33 +0800

    #5

--- views/Landing/index.tsx
+++ views/Landing/index.tsx
@@ -1,36 +1,30 @@
 import styled from "styled-components";
-import Link from "next/link";
 
-import productCategories from "data/productCategories";
-import CategoryItem from "components/CategoryItem";
+import ProductCategory from "./ProductCategory";
 
 const Landing: React.FC = () => {
-    return (
-        <div>
-            <CategoryContainer>
-                {productCategories.map((category) => (
-                    <Link
-                        key={category.value}
-                        href={`/search?category=${category.value}`}
-                    >
-                        <CategoryItem
-                            id={category.value}
-                            imageUrl={category.url}
-                            name={category.name}
-                            size="sm"
-                            caterMobile={true}
-                        />
-                    </Link>
-                ))}
-            </CategoryContainer>
+    return <div>
+        <ProductCategory />
+        <div
+            style={{
+                width: "100%",
+                height: "25rem",
+                backgroundColor: "gainsboro",
+            }}
+        >
+            Some other section
         </div>
-    );
+        <div
+            style={{
+                width: "100%",
+                height: "25rem",
+                backgroundColor: "pink",
+                marginTop: "2rem",
+            }}
+        >
+            Some other section
+        </div>
+    </div>
 };
 
-const CategoryContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  row-gap: 1rem;
-  column-gap: 1rem;
-`;
 export default Landing;
