f9787cecb7a70f35f0545180ac23fa706812d2ad
vinceleong <61788459+vinceleong@users.noreply.github.com>
2023-01-08 19:20:59 +0800

    minor ui change in landing page

--- views/Landing/index.tsx
+++ views/Landing/index.tsx
@@ -5,22 +5,18 @@ import ProductCategory from "./ProductCategory";
 const Landing: React.FC = () => {
     return <div>
         <ProductCategory />
-        <div
-            style={{
-                width: "100%",
-                height: "25rem",
-                backgroundColor: "gainsboro",
-            }}
-        />
-        <div
-            style={{
-                width: "100%",
-                height: "25rem",
-                backgroundColor: "pink",
-                marginTop: "2rem",
-            }}
-        />
+        <DummyContent color="gainsboro" />
+        <DummyContent color="pink" />
     </div>
 };
+
+const DummyContent = styled.div<{ color: string }>(({ theme, ...p }) => `
+    width: 100%;
+    height: 25rem;
+    background-color: ${p.color};
+    margin-top: 2rem;
+    border-radius: ${theme.borderRadius.md};
+`)
+
 
 export default Landing;
